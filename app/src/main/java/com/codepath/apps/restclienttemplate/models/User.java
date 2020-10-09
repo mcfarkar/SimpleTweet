7cc429594ec4db0f66491aad2311a7d86c4768ec
mcfarkar <mcfarkar@oregonstate.edu>
2020-10-09 16:27:36 -0700

    Initial commit

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
@@ -0,0 +1,25 @@
+package com.codepath.apps.restclienttemplate.models;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by mcfarkar on 08,October,2020
+ */
+public class User {
+
+    public String name;
+    public String screenName;
+    public String profileImageURL;
+
+
+
+    public static User fromJson(JSONObject jsonObject) throws JSONException {
+        User user = new User();
+        user.name = jsonObject.getString("name");
+        user.screenName = jsonObject.getString("screen_name");
+        user.profileImageURL = jsonObject.getString("profile_image_url_https");
+        return user;
+
+    }
+}
